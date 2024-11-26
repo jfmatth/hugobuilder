81fb4d5d3897ce0379c36bffc7b2a01293e0ed1e
John Matthew <john@compunique.com>
2024-11-26 12:18:01 -0800

    use github registry and login

--- build.sh
+++ build.sh
@@ -1,3 +1,8 @@
-export version=v0.1
-docker build . -t jfmatth/hugobuilder:$version
-docker push jfmatth/hugobuilder:$version
+export version=v0.2
+# docker build . -t jfmatth/hugobuilder:$version
+# docker push jfmatth/hugobuilder:$version
+
+echo "Logging into gibhub container registry, use PAT"
+podman login --username jfmatth ghcr.io
+podman  build . -t ghcr.io/jfmatth/hugobuilder:$version
+podman  push ghcr.io/jfmatth/hugobuilder:$version
