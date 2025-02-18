ceff9742192947742d12d61c2ba933f16bec8a4f
John Matthew <john@compunique.com>
2025-02-18 06:35:19 -0800

    Clean up local build scripts, change to build only local don't push

--- build.bat
+++ build.bat
@@ -1,9 +1,5 @@
 set /p version=<VERSION
 set image=ghcr.io/jfmatth/hugobuilder
-@REM # docker build . -t jfmatth/hugobuilder:$version
-@REM # docker push jfmatth/hugobuilder:$version
 
-@REM # Podman login credentials in ~/.config/containers/auth.json (https://docs.podman.io/en/latest/markdown/podman-login.1.html#examples)
 podman login ghcr.io
-podman  build . -t %image%:%version%
-podman  push %image%:%version%
+podman build . -t %image%:%version%
--- build.sh
+++
@@ -1,8 +0,0 @@
-export version=v0.139.3
-# docker build . -t jfmatth/hugobuilder:$version
-# docker push jfmatth/hugobuilder:$version
-
-# Podman login credentials in ~/.config/containers/auth.json (https://docs.podman.io/en/latest/markdown/podman-login.1.html#examples)
-podman login ghcr.io
-podman  build . -t ghcr.io/jfmatth/hugobuilder:$version
-podman  push ghcr.io/jfmatth/hugobuilder:$version
